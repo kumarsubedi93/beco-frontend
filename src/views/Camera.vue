e41d61377e4360b9f575320e0ee0c36fb05a81a8
Kumar <subedi99635@gmail.com>
2023-08-15 14:28:23 +0545

    photo choser changed

--- src/views/Camera.vue
+++ src/views/Camera.vue
@@ -1,8 +1,9 @@
 <template>
     <div class="wraper">
         <div class="garbage-flex">
-             <template v-if="showCamera">
-                <camera @started="handleCameraStarted" @loading="handleCameraLoading" :resolution="{ width: 400, height: 400 }" ref="cameraRef" autoplay>
+            <template v-if="showCamera">
+                <camera @started="handleCameraStarted" @loading="handleCameraLoading"
+                    :resolution="{ width: 400, height: 400 }" ref="cameraRef" autoplay>
                 </camera>
             </template>
             <template v-else>
@@ -10,26 +11,39 @@
                 <img v-else :src="previewUrl" alt="camera icon" />
             </template>
         </div>
+
         <div class="click-to-begin">
             <div class="click-begin">
                 <template v-if="showCamera && showSnapBtn">
-                    <v-btn @click="handleTakeSnapshoot" class="submit-btn" size="large"> Create snapshot !! </v-btn> 
+                    <v-btn @click="handleTakeSnapshoot" class="submit-btn" size="large"> Create snapshot !! </v-btn>
                 </template>
                 <template v-else>
                     <h2> Click To Begin </h2>
-                    <img v-if="!loading" src="/start-buttonpng.png" @click="openFileExploreOrCamera" alt="start button" />
-                    <br>
+                    <v-menu v-if="!loading">
+                        <template v-slot:activator="{ props }">
+                            <img v-bind="props" src="/start-buttonpng.png"  alt="start button" />
+                        </template>
+                        <v-list>
+                            <v-list-item v-for="(item, index) in photoChoseOptions" :key="index">
+                                <v-list-item-title class="pointer-cursor" @click="openFileExploreOrCamera(index)">
+                                    {{ item }}
+                                </v-list-item-title>
+                            </v-list-item>
+                        </v-list>
+                    </v-menu>
                 </template>
             </div>
             <input type="file" ref="fileInput" v-on:change="handleFileInput" class="hidden-file" />
-            <h2 v-if="resultTxt" class="font-weight-bold"> The result is : <span style="color: green;"> {{ resultTxt.name }} </span>
-             <br> 
-             <span> Total time to get result is: {{ timeTakenby - 0.5 }}s  </span>
+            <h2 v-if="resultTxt" class="font-weight-bold"> The result is : <span style="color: green;"> {{ resultTxt.name }}
+                </span>
+                <br>
+                <span> Total time to get result is: {{ timeTakenby - 0.5 }}s </span>
             </h2>
 
             <div class="btn">
-                <img v-if="loading" src="/VZvw.gif" alt="loading"> 
-                <v-btn v-if="fileValue && !loading" class="submit-btn" @click="handleGetResult" size="large"> Submit </v-btn>
+                <img v-if="loading" src="/VZvw.gif" alt="loading">
+                <v-btn v-if="fileValue && !loading" class="submit-btn" @click="handleGetResult" size="large"> Submit
+                </v-btn>
             </div>
 
 
@@ -58,11 +72,15 @@ const showCamera = ref(false)
 const cameraRef = ref();
 const showSnapBtn = ref(false)
 const loadingCamera = ref(false)
+const photoChoseOptions = ref([
+    'Photo Library',
+    'Take Photo'
+])
 
 const isMobileDevice = () => {
-   const userAgent = navigator.userAgent.toLowerCase();
-   const mobileKeywords = ['android', 'webos', 'iphone', 'ipad', 'ipod', 'blackberry', 'windows phone'];
-   return mobileKeywords.some(keyword => userAgent.includes(keyword));
+    const userAgent = navigator.userAgent.toLowerCase();
+    const mobileKeywords = ['android', 'webos', 'iphone', 'ipad', 'ipod', 'blackberry', 'windows phone'];
+    return mobileKeywords.some(keyword => userAgent.includes(keyword));
 }
 
 const resetAllValues = () => {
@@ -82,7 +100,7 @@ const handleCameraLoading = () => {
     loadingCamera.value = true
 }
 
-const handleTakeSnapshoot = async () =>  {
+const handleTakeSnapshoot = async () => {
     console.log(cameraRef)
     const blob = await cameraRef.value?.snapshot(
         { width: 400, height: 400 },
@@ -97,52 +115,52 @@ const handleTakeSnapshoot = async () =>  {
 
 
 const handleFileInput = (e) => {
-   fileValue.value = e.target.files[0]
-   previewUrl.value = URL.createObjectURL(e.target.files[0])
+    fileValue.value = e.target.files[0]
+    previewUrl.value = URL.createObjectURL(e.target.files[0])
 }
 
-const openFileExploreOrCamera = () => {
-    if(isMobileDevice()){
-      resetAllValues() 
-      showCamera.value = true
-    }else {
+const openFileExploreOrCamera = (key) => {
+    if (key == 1) {
+        resetAllValues()
+        showCamera.value = true
+    } else {
         fileInput.value.click()
-    }    
+    }
 }
 
 const handleGetResult = () => {
-   if (!fileValue) return; 
-   const url = "http://35.208.18.220/uploads"
-   const form = new FormData()
-   form.append('image', fileValue.value)
-   const startTime = new Date();
-   loading.value = true
-   axios.post(url, form, {
-    headers :{
-        'Content-Type': 'multipart/form-data'
-    }
-   }).then(res => {
-     const endTime = new Date();
-     const timeTaken = (endTime - startTime) /1000;
-     timeTakenby.value = timeTaken
-     const result  = mapJsonOutput(res.data.results)
-     resultTxt.value = result[0]
-     loading.value = false
-   }).catch(err => {
-    loading.value = false
-   })  
+    if (!fileValue) return;
+    const url = "http://35.208.18.220/uploads"
+    const form = new FormData()
+    form.append('image', fileValue.value)
+    const startTime = new Date();
+    loading.value = true
+    axios.post(url, form, {
+        headers: {
+            'Content-Type': 'multipart/form-data'
+        }
+    }).then(res => {
+        const endTime = new Date();
+        const timeTaken = (endTime - startTime) / 1000;
+        timeTakenby.value = timeTaken
+        const result = mapJsonOutput(res.data.results)
+        resultTxt.value = result[0]
+        loading.value = false
+    }).catch(err => {
+        loading.value = false
+    })
 }
 
 const mapJsonOutput = (result) => {
     const maps = ['Ewaste', 'Glass', 'Metal', 'NonRecyclable', 'Paper', 'Plastic']
     const newResult = result.map((item, key) => {
-            return {
-                name:maps[key],
-                value:item
-            }
-    }) 
+        return {
+            name: maps[key],
+            value: item
+        }
+    })
     return newResult.sort((a, b) => {
-        if(a.value > b.value) {
+        if (a.value > b.value) {
             return -1
         }
         return 1
@@ -211,5 +229,9 @@ p.text-tiny {
     fill: #637fb0;
     font-size: 34px;
     margin: 0px 15px 15px 15px;
+}
+
+.pointer-cursor{
+    cursor: pointer;
 }
 </style>
